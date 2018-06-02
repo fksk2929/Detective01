536149c271838b939fec1d1080b10b4b283173ef
fksk2929 <39858899+fksk2929@users.noreply.github.com>
2018-06-03 00:39:51 +0900

    20180602

---
+++ js/plugins/InfoWindow.js
@@ -0,0 +1,92 @@
+﻿//=============================================================================
+// InfoWindow.js
+//=============================================================================
+
+/*:
+ * @plugindesc 情報表示ウィンドウをメニュー画面に追加するプラグイン
+ * @author Me
+ *
+ * @help 情報表示ウィンドウをメニュー画面上に追加します。
+ *
+ */
+
+(function() {
+
+// マップ上にウインドウ表示宣言
+var Scene_map_start = Scene_Map.prototype.start;
+Scene_Map.prototype.start = function() {
+Scene_map_start.call(this);
+this._InfomWindow = new Window_Infom();
+this.addWindow(this._InfomWindow);
+};
+var _Scene_Map_update = Scene_Map.prototype.update;
+Scene_Map.prototype.update = function() {
+_Scene_Map_update.call(this);
+this._InfomWindow.setText();
+ if($gameSwitches.value(35)){
+     this._InfomWindow.show();
+ }else{
+     this._InfomWindow.hide();
+ }
+};
+
+// ウインドウ作成
+function Window_Infom() {
+this.initialize.apply(this, arguments);
+}
+
+Window_Infom.prototype = Object.create(Window_Base.prototype);
+Window_Infom.prototype.constructor = Window_Infom;
+Window_Infom.prototype.initialize = function() {
+var x = 10;
+var y = 10;
+var width = 146;
+var height = 60;
+Window_Base.prototype.initialize.call(this, x, y, width, height);
+};
+
+Window_Infom.prototype.setText = function(str) {
+this._text = str;
+this.refresh();
+};
+
+// テキスト表示
+Window_Infom.prototype.refresh = function() {
+this.contents.clear();
+
+ if($gameVariables.value(10) == 5){
+     this.drawText("★★★★★",0,0);
+ }else if($gameVariables.value(10) == 4){
+     this.drawText("★★★★",0,0);
+ }else if($gameVariables.value(10) == 3){
+     this.drawText("★★★",0,0);
+ }else if($gameVariables.value(10) == 2){
+     this.drawText("★★",0,0);
+ }else if($gameVariables.value(10) == 1){
+     this.drawText("★",0,0);
+ }else if($gameVariables.value(10) <= 0){
+     this.drawText("",0,0);
+ }else{
+     this.drawText("エラー",0,0);
+ }
+
+};
+
+// フォントサイズ
+Window_Infom.prototype.standardFontSize = function() {
+return 24;
+};
+// ウインドウの透明度
+Window_Infom.prototype.standardBackOpacity = function() {
+return 255;
+};
+// ウインドウの余白
+Window_Infom.prototype.standardPadding = function() {
+return 12;
+};
+// ウインドウの色調
+Window_Infom.prototype.updateTone = function() {
+this.setTone(0, 0, 0);
+};
+
+})();
---
+++ readme.txt
@@ -0,0 +1,21 @@
+●権利表記： 「c2015 KADOKAWA CORPORATION./YOJI OJIMA」
+
+★プラグイン
+「TMEquipSlotEX」
+「TMOmitEquipCommand」
+「TMItemCategoryEx」
+　作者：tomoaky様
+　http://hikimoki.sakura.ne.jp/
+　
+「InfoWindow」
+　作者：Me様
+　http://ktnhmv.jugem.jp/?eid=21
+
+「aligncenter」
+　作者：村人A様
+　http://rpgmaker-script-wiki.xyz/index.php
+
+★効果音
+「大型船の汽笛1」
+　作者：効果音ラボ　Killy様
+　https://soundeffect-lab.info/
